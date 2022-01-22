8df3ae3e9ef425656f7d382c266bfd073087982c
cutdred1 <mec.satnitla@gmail.com>
2022-01-22 14:29:57 +0100

    Selenium JUnit Projects

---
+++ src/test/java/day06_JUnit/C02_AramaTesti.java
@@ -0,0 +1,59 @@
+package day06_JUnit;
+
+import com.sun.source.tree.IfTree;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C02_AramaTesti {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(13));
+
+        //1. “https://www.saucedemo.com” Adresine gidin
+        driver.get("https://www.saucedemo.com");
+        //2. Username kutusuna “standard_user” yazdirin
+        WebElement usernameKutusu= driver.findElement(By.xpath("//input[@id='user-name']"));
+        usernameKutusu.sendKeys("standard_user");
+
+        //3. Password kutusuna “secret_sauce” yazdirin
+        WebElement passwordKutusu= driver.findElement(By.xpath("//input[@id='password']"));
+        passwordKutusu.sendKeys("secret_sauce");
+
+        //4. Login tusuna basin
+        driver.findElement(By.xpath("//input[@id='login-button']")).click();
+
+        //5. Ilk urunun ismini kaydedin ve bu urunun sayfasina gidin
+        WebElement ilkUrunElementi= driver.findElement(By.xpath("(//div[@class='inventory_item_name'])[1]"));
+        String ilkUrunIsmi= ilkUrunElementi.getText();
+        ilkUrunElementi.click();
+
+        //6. Add to Cart butonuna basin
+        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
+
+        //7. Alisveris sepetine tiklayin
+        driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
+
+        //8. Sectiginiz urunun basarili olarak sepete eklendigini control edin
+        WebElement sepette= driver.findElement(By.xpath("//div[@class='inventory_item_name']"));
+        String sepetItemName= sepette.getText();
+        if (ilkUrunIsmi.equals(sepetItemName)){
+            System.out.println("Urun sepetete PASSED");
+        }else {
+            System.out.println("Urun sepetete FAILED");
+        }
+
+        //9. Sayfayi kapatin
+        driver.close();
+
+
+
+
+    }
+}
